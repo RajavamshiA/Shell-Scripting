07eed78486a37f54d5a037bbe526ffbd6ff79787
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-15 20:09:28 +0000

    Installation code in script

--- 09.install-script.sh
+++ 09.install-script.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR::You must have sudo access to execute this script"
     exit 1 #other than 0
@@ -10,10 +10,10 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0]
+if [ $? -ne 0 ]
 then # not installed
     dnf install mysql -y
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then 
         echo "Installing Mysql... Failure"
     else
@@ -27,10 +27,10 @@ fi
 
 dnf list installed git
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then # not installed
     dnf install git -y
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then 
         echo "Installing Git... Failure"
     else
