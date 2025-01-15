382c73b983c77d33e559b2fb6cc63ef104068f76
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-15 17:46:02 +0000

    Arrays example

---
+++ 07.arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Movies=("rrr" "devara" "pushpa")
+
+echo "First Movie is: ${Movies[0]}"
+echo "Second Movie is: ${Movies[1]}"
+echo "Third Movie is: ${Movies[2]}"
+
+echo "All Movies are:${Movies[@]}"
