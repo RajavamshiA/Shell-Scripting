b6a1c810553a0d130698e94780df9f51e3936cbb
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-15 18:00:45 +0000

    Special Variables

---
+++ 08.special-var.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script Name: $0"
+echo "Present Working Directory: $PWD"
+echo "Home directory of Current User: $HOME"
+echo "Which user is running this script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
+
+
+
+
