cff79c577dcc226734e1c70723a163c7aeeec79c
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-16 21:36:39 +0000

    logs Example 2

--- 13.logs.sh
+++ 13.logs.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/logs/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -13,13 +14,15 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2... $R Failure"
+        echo -e "$2... $R Failure $N"
     else
-        echo -e "$2... $G Success"
+        echo -e "$2... $G Success $N"
     fi
 }
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [$USERID -ne 0 ]
 then
     echo "ERROR::You must have sudo access to execute this script"
     exit 1 #other than 0
@@ -33,7 +36,7 @@ then # not installed
     dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Mysql"
 else
-    echo -e "Mysql is already $Y installed"
+    echo -e "Mysql is already $Y installed $N"
 fi
 
 
@@ -45,5 +48,5 @@ then # not installed
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Git"
 else
-    echo -e "Git is already $Y installed"
+    echo -e "Git is already $Y installed $N"
 fi
