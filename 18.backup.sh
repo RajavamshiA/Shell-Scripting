5883f269458f277b51ac1fed1831acebb49be208
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-21 01:49:31 +0000

    backup

--- 18.backup.sh
+++ 18.backup.sh
@@ -51,5 +51,9 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) 
 
-echo "Files are: $FILES"
-
+if [ -n "$FILES" ]
+then
+    echo "Files are: $FILES"
+else
+    echo "No files are found older than $DAYS"
+fi
