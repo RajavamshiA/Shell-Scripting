4b9302e4d61f051ae72fba85d3623dce1fd7ba44
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-21 01:19:05 +0000

    while loop added

--- 18.backup.sh
+++ 18.backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3,-14} # If user is not providing the number of days, we are taking 14 as default. 
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs/"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -25,12 +25,29 @@ VALIDATE(){
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 18.backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
 }
+
+mkdir -p /home/ec2-user/shellscript-logs/
 
 if [ $# -lt 2]
 then   
     USAGE
 fi
 
+if [ ! -d $SOURCE_DIR]
+then
+    echo "$SOURCE_DIR  DOes not Exist.. Please Check"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR]
+then
+    echo "$DEST_DIR  DOes not Exist.. Please Check"
+    exit 1
+fi
+
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES=$(find $SOURCE_DIR -name "*.log" +mtime $DAYS)
 
