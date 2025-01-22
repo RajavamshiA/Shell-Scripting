bed9302e9e96b89abb17cd2eb4e7756bfe7dd971
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-01-22 10:24:53 +0000

    Disk Usage

---
+++ 19.disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THERSHOLD=5 # real projects monitor for 70
+MSG=""
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THERSHOLD ]
+    then 
+        MSG+="High disk usage on partition: $PARTITION Usage is: $USAGE <br>"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message:$MSG"
